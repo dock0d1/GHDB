2a9ba29562bf3bb1a7a011a6b42aa724f3a6dbd0
dock <43358190+dock0d1@users.noreply.github.com>
2021-10-21 14:08:52 -0300

    Create ghdb.sh

---
+++ ghdb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ "$1" == "" ]
+then
+    echo
+    echo Script feito pra automatizar o processo de dork
+    echo Modo de uso : ghdb.sh www.facebook.com
+    echo Criado por dock0d1
+    echo 
+    echo
+    exit
+fi
+
+SEARCH="google-chrome-stable"
+ALVO="$1"
+echo "Pesquisa no Pastebin"
+$SEARCH "http://google.com/search?q=site:pastebin.com+$ALVO" 2> /dev/null
+echo "Pesquisa no Trello"
+$SEARCH	"http://google.com/search?q=site:pastebin.com+$ALVO" 2> /dev/null
+echo "Pesquisa por arquivos"
+$SEARCH "http://google.com/search?q=site:$ALVO+ext:php+OR+ext:asp+OR+ext:txt" 2>/dev/null
